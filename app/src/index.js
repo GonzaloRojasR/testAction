d7bea5b1610854c09efaf22ac5f2b01034ac8c54
GonzaloRojasR <g.rojasrojas3@uandresbello.edu>
2025-04-15 00:47:42 -0400

    update

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/desafio', (req, res) => {
+app.get("/desafio", (req, res) => {
   res.send('¡Hola Mundo!');
 });
 
 if (require.main === module) {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+    console.log("Servidor corriendo en http://localhost:${port}");
   });
 }
 
